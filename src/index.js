07824f101aca28cdfeda157661282a7f7349dea7
surendrakanigiri <surendra@krify.net>
2024-01-11 09:46:30 +0530

    Added routes, error handler and handling invalid methods

--- src/index.js
+++ src/index.js
@@ -2,9 +2,25 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const CONFIG = require("./config");
 
 // Middleware to parse JSON data in request body
 app.use(express.json());
+
+//Error Handler configure
+const  errorHandler = require('./middleware/errorHandler');
+app.use(errorHandler);
+
+// Routes forwarding
+const routes = require("./routes");
+app.use("/", routes);
+
+// Catch-all route for non-existent routes
+app.use((req, res) => {
+    res.status(CONFIG.ERROR_STATUS_CODES.methodInvalidErrorCode).send(CONFIG.ERROR_MESSAGES.methodInvalidErrorMessage);
+    return;
+});
+
 
 // PORT CONFIG
 const server = app.listen( PORT, () => {
