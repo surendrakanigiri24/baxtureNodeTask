e99c948816647a0877ecb6e11c7fe7d957480e2f
surendrakanigiri <surendra@krify.net>
2024-02-14 08:42:35 +0530

    Added db config values

--- src/config.js
+++ src/config.js
@@ -2,6 +2,13 @@
 
 const CONFIG = {
 
+    // DB config
+    DB_NAME:process.env.DB_NAME,
+    DB_PASS:process.env.DB_PASS,
+    DB_USER:process.env.DB_USER,
+    DB_HOST:process.env.DB_HOST,
+    DB_PORT:process.env.DB_PORT,
+
     // ERROR STATUS CODES
     ERROR_STATUS_CODES:{
         internalErrorCode: 500,
