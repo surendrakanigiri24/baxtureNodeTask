78e42cd5a2a22d582d558d4cc2235c2496124a18
surendrakanigiri <surendra@krify.net>
2024-02-14 15:30:53 +0530

    Created clients for db config

---
+++ src/clients/db/models/files.model.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
+const fileModel = sequelize.define('files',{
+    file_id:{
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4
+    },
+    file_name:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    created_date: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false
+    },
+    buffer: {
+        type: DataTypes.BLOB('long'),
+        allowNull: false
+    }
+});
+
+module.exports = fileModel;
+
+
